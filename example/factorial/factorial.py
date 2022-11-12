c5c899634e3de56e631bf5983538123a86edf531
DrEamOAO <sata031818@gmail.com>
2022-11-12 18:36:07 +0800

    add test_factorial.py

--- example/factorial/factorial.py
+++ example/factorial/factorial.py
@@ -1,5 +1,8 @@
-def factorial(n);
+def factorial(n):
     r = 1
-    for i in range(1,n+1);
+    for i in range(1,n+1):
         r *= i
     return r
+
+if __name__=='__main__':
+    print('factorial(10)=',factorial(10))
