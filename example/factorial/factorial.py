521607ea218b67556fe7f24eca9078f6f99f4ccc
DrEamOAO <sata031818@gmail.com>
2022-11-13 15:28:09 +0800

    flow pass

--- example/factorial/factorial.py
+++ example/factorial/factorial.py
@@ -3,6 +3,13 @@ def factorial(n):
     for i in range(1,n+1):
         r *= i
     return r
+def short(n):
+    r=0
+    for i in range(1,n+1):
+        r+=i
+    return r
 
 if __name__=='__main__':
     print('factorial(10)=',factorial(10))
+    print('short(10)=',short(10))
+#分支測試
--- example/factorial/test_factorial.py
+++ example/factorial/test_factorial.py
@@ -2,3 +2,6 @@
 from factorial import *
 def test_factorial():
     assert factorial(5) == 120
+def test_short():
+    assert short(10) == 55    
+#分支測試
